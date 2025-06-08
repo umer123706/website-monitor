ada6cde602bab09c04114ee3bca07849a7703ccb
umer123706 <umerlatif919@gmail.com>
2025-06-08 08:06:26 +0500

    Update website_monitor.py

--- website_monitor.py
+++ website_monitor.py
@@ -17,7 +17,6 @@ EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")  # From GitHub Secrets
 TO_EMAILS = [
     "umer@technevity.net",
     "hafiz@technevity.net",
-    "l2@technevity.net",
 ]
 
 # List of URLs to monitor
